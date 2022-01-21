d2a979e2033647d6a04c6f8c76cc12a064bf7415
kasoqian <kasoqian@mugenart.io>
2022-01-22 03:57:52 +0800

    feat: add app framework

--- main.js
+++ main.js
@@ -5,11 +5,12 @@ const path = require("path");
 function createWindow() {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 1260,
-    height: 1240,
+    width: 540,
+    height: 960,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
+    icon: "./public/bugoo.png",
   });
 
   // and load the index.html of the app.
