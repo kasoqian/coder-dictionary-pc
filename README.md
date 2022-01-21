bd86c47e1c5dcc06ba88e034053499c7555facf7
kasoqian <kasoqian@mugenart.io>
2022-01-22 03:22:02 +0800

    refactor: first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# coder-dictionary-pc
+
+pc 版本，为简单快速，使用套壳的方式进行开发
---
+++ vue/vue.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  publicPath: "./", // 文件加载设置为相对路径
+  outputDir: "dist",
+  lintOnSave: false, // 关闭eslint
+  productionSourceMap: true, // 生产环境下css 分离文件
+};
