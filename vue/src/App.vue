9ad128a1aa49a624073ac3a1452bdb70574e4994
kasoqian <kasoqian@mugenart.io>
2022-01-22 03:28:47 +0800

    Revert "docs: update readme"
    This reverts commit c4d5def24f006018e3090f30ed5d6bb436fae49f.

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,143 +1,26 @@
 <template>
-  <div class="container">
-    <div>
-      <h1>{{ wordList[index].word }}</h1>
-    </div>
-    <div>
-      <p style="font-size: 14px">
-        {{ wordList[index].chinese }}
-      </p>
-    </div>
-    <div style="margin: 50px 0">
-      <input
-        class="myanswer"
-        :class="
-          wordList[index].word == wordconfirm
-            ? 'green'
-            : 'red'
-        "
-        type="text"
-        @input="confirmword"
-      />
-      <div>
-        <h3 style="font-size: 2rem; line-height: 1">
-          {{
-            wordList[index].word == wordconfirm
-              ? "✅"
-              : "coding...."
-          }}
-        </h3>
-      </div>
-    </div>
-    <div style="margin: auto">
-      <audio
-        style="width: 80%"
-        :src="wordList[index].online"
-        controls="controls"
-        autoplay
-      ></audio>
-    </div>
-  </div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
+
 <script>
-const words = require("./assets/words.json");
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  data() {
-    return {
-      played: [],
-      wordList: [],
-      index: 0,
-      wordconfirm: false,
-      wordInput: "",
-    };
-  },
-  created() {
-    this.wordList = words;
-    this.randomIndex();
-
-    window.document.onkeydown = this.checkWords;
-  },
-  methods: {
-    randomIndex() {
-      const len = this.wordList.length;
-      const random = Math.floor(Math.random() * len + 1);
-      this.index = random;
-      this.played.push(random);
-    },
-    checkWords(evt) {
-      this.wordconfirm = "";
-      switch (evt.code) {
-        case "ArrowUp":
-          this.randomIndex();
-          break;
-        case "ArrowDown":
-          this.backWord();
-          break;
-        case "ArrowRight":
-          this.randomIndex();
-          break;
-        case "ArrowLeft":
-          this.backWord();
-          break;
-      }
-    },
-    async confirmword(e) {
-      this.wordconfirm = e.target.value;
-      if (this.wordList[index].word == this.wordconfirm) {
-        let that = this;
-        setTimeout(() => that.randomIndex(), 2000);
-      }
-    },
-    backWord() {
-      let last = this.played.length - 2;
-      if (last <= 0) this.index = this.played[0];
-      if (last > 0) {
-        this.index = this.played[last];
-        this.played = this.played.slice(0, last + 1);
-      }
-    },
-  },
-};
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
 </script>
+
 <style>
-body {
-  background: rgb(237, 237, 237);
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  font-family: "Gill Sans", "Gill Sans MT", Calibri,
-    "Trebuchet MS", sans-serif;
-  line-height: 2;
-}
-.container {
-  background-image: linear-gradient(
-    to top,
-    rgb(238, 195, 201) 0%,
-    #e7627d 20%,
-    #b8235a 39%,
-    #801357 51%,
-    #3d1635 74%,
-    #1c1a27 100%
-  );
-  color: white;
-  padding: 20px 10px;
-  border-radius: 10px;
-  height: 90vh;
-  box-shadow: 3px 3px 6px rgb(194, 194, 194);
-}
-.myanswer {
-  width: 90%;
-  height: 50px;
-  border: none;
-  border-radius: 10px;
-  font-size: 30px;
-  text-align: center;
-  font-family: monospace;
-  font-weight: 800;
-}
-.green {
-  color: green;
-}
-.red {
-  color: grey;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
