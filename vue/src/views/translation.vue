0a93e1193f96df9e6ee4c8db9566b2a593a0af71
kasoqian <kasoqian@mugenart.io>
2022-01-24 12:25:10 +0800

    feat: add router

---
+++ vue/src/views/translation.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="about container">
+    <h1>This is an aboudsadsat page</h1>
+  </div>
+</template>
+
+<style>
+.container {
+  background-image: linear-gradient(
+    to top,
+    rgb(238, 195, 201) 0%,
+    #e7627d 20%,
+    #b8235a 39%,
+    #801357 51%,
+    #3d1635 74%,
+    #1c1a27 100%
+  );
+  color: white;
+  padding: 20px 10px;
+  border-radius: 10px;
+  height: 90vh;
+  box-shadow: 3px 3px 6px rgb(194, 194, 194);
+}
+</style>
